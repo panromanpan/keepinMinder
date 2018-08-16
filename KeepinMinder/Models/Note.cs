72b49d000505e7e2608b77eb3cbfe9f59c85c49b
panromanpan <roman.viytovych@gmail.com>
2018-08-16 16:18:27 +0300

    Added Notes

---
+++ KeepinMinder/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeepinMinder.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        public IdentityUser User { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+}
